bb89f47911f858ef15e98ee4e0eabb165d316b9e
Nathaniel Hartlen <nwhartlen@icloud.com>
2024-05-17 13:24:59 -0300

    real time clock driver

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -3,6 +3,7 @@
 #include "system.h"
 #include "x86.h"
 volatile uint32_t CountDown;
+#define CURRENT_YEAR        2024                            // Change this each year its jank but hey!
 #define FB_COMMAND_PORT         0x3D4
 #define FB_DATA_PORT            0x3D5
 #define COUNTDOWN_DONE_MSG 1
@@ -958,11 +959,22 @@ static void play_sound(int nFrequence) {
  TIME AND CLOCK FUNCTION DRIVER
  
  */
-void timer_handler(struct regs *a_r){
+ int century_register = 0x00;                                // Set by ACPI table parsing code if possible
+
+ unsigned char second;
+unsigned char minute;
+unsigned char hour;
+unsigned char day;
+unsigned char month;
+unsigned int year;
+void pic_timer_handler(struct regs *a_r){
 	
  }
-void timer_install(){
-	irq_install(0,timer_handler);
+void cmos_timer_handler(struct regs *a_r){
+	
+}
+void timer_pic_install(){
+	irq_install(0,pic_timer_handler);
  }
  
  void timer_sleep_cycles(int waits){
@@ -972,9 +984,143 @@ void timer_install(){
 		__asm("	nop \r\n");
 	 }
  }
+ void cmos_timer_install(){
+	 irq_install(8,cmos_timer_handler);
+ }
+ WriteTOCMOS(unsigned char array[])
+{
+   unsigned char index;
  
+   for(index = 0; index < 128; index++)
+   {
+      unsigned char tvalue = array[index];
+      _asm
+      {
+         cli             /* Clear interrupts*/
+         mov al,index    /* move index address*/
+         out 0x70,al     /* copy address to CMOS register*/
+         /* some kind of real delay here is probably best */
+         mov al,tvalue   /* move value to al*/
+         out 0x71,al     /* write 1 byte to CMOS*/
+         sti             /* Enable interrupts*/
+      }
+   }
+}
+ enum {
+      cmos_address = 0x70,
+      cmos_data    = 0x71
+};
  
+int get_update_in_progress_flag() {
+      outb(cmos_address, 0x0A);
+      return (inb(cmos_data) & 0x80);
+}
  
+unsigned char get_RTC_register(int reg) {
+      outb(cmos_address, reg);
+      return inb(cmos_data);
+}
+ 
+void read_rtc() {
+      unsigned char century;
+      unsigned char last_second;
+      unsigned char last_minute;
+      unsigned char last_hour;
+      unsigned char last_day;
+      unsigned char last_month;
+      unsigned char last_year;
+      unsigned char last_century;
+      unsigned char registerB;
+ 
+      // Note: This uses the "read registers until you get the same values twice in a row" technique
+      //       to avoid getting dodgy/inconsistent values due to RTC updates
+ 
+      while (get_update_in_progress_flag());                // Make sure an update isn't in progress
+      second = get_RTC_register(0x00);
+      minute = get_RTC_register(0x02);
+      hour = get_RTC_register(0x04);
+      day = get_RTC_register(0x07);
+      month = get_RTC_register(0x08);
+      year = get_RTC_register(0x09);
+      if(century_register != 0) {
+            century = get_RTC_register(century_register);
+      }
+ 
+      do {
+            last_second = second;
+            last_minute = minute;
+            last_hour = hour;
+            last_day = day;
+            last_month = month;
+            last_year = year;
+            last_century = century;
+ 
+            while (get_update_in_progress_flag());           // Make sure an update isn't in progress
+            second = get_RTC_register(0x00);
+            minute = get_RTC_register(0x02);
+            hour = get_RTC_register(0x04);
+            day = get_RTC_register(0x07);
+            month = get_RTC_register(0x08);
+            year = get_RTC_register(0x09);
+            if(century_register != 0) {
+                  century = get_RTC_register(century_register);
+            }
+      } while( (last_second != second) || (last_minute != minute) || (last_hour != hour) ||
+               (last_day != day) || (last_month != month) || (last_year != year) ||
+               (last_century != century) );
+ 
+      registerB = get_RTC_register(0x0B);
+ 
+      // Convert BCD to binary values if necessary
+ 
+      if (!(registerB & 0x04)) {
+            second = (second & 0x0F) + ((second / 16) * 10);
+            minute = (minute & 0x0F) + ((minute / 16) * 10);
+            hour = ( (hour & 0x0F) + (((hour & 0x70) / 16) * 10) ) | (hour & 0x80);
+            day = (day & 0x0F) + ((day / 16) * 10);
+            month = (month & 0x0F) + ((month / 16) * 10);
+            year = (year & 0x0F) + ((year / 16) * 10);
+            if(century_register != 0) {
+                  century = (century & 0x0F) + ((century / 16) * 10);
+            }
+      }
+ 
+      // Convert 12 hour clock to 24 hour clock if necessary
+ 
+      if (!(registerB & 0x02) && (hour & 0x80)) {
+            hour = ((hour & 0x7F) + 12) % 24;
+      }
+ 
+      // Calculate the full (4-digit) year
+ 
+      if(century_register != 0) {
+            year += century * 100;
+      } else {
+            year += (CURRENT_YEAR / 100) * 100;
+            if(year < CURRENT_YEAR) year += 100;
+      }
+}
+ReadFromCMOS (unsigned char array [])
+{
+   unsigned char tvalue, index;
+ 
+   for(index = 0; index < 128; index++)
+   {
+      _asm
+      {
+         cli             /* Disable interrupts*/
+         mov al, index   /* Move index address*/
+         /* since the 0x80 bit of al is not set, NMI is active */
+         out 0x70,al     /* Copy address to CMOS register*/
+         /* some kind of real delay here is probably best */
+         in al,0x71      /* Fetch 1 byte to al*/
+         sti             /* Enable interrupts*/
+         mov tvalue,al
+       }
+ 
+       array[index] = tvalue;
+   }
+}
  
  
  
